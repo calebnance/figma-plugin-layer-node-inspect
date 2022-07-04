b3c54d706e9633a9777201e0f67a65cc22a29baa
calebnance <calebnance@gmail.com>
2022-07-03 22:37:07 -0400

    simple ping pong

--- src/code.js
+++ src/code.js
@@ -3,31 +3,28 @@ figma.showUI(__html__, { height: 400, width: 380, themeColors: true });
 
 console.clear();
 
+let listenForSelection = true;
+
+figma.on('run', () => {
+  console.log('plugin started');
+});
+
+// listener on selection change
+// https://www.figma.com/plugin-docs/api/properties/figma-on/
+figma.on('selectionchange', () => {
+  // only listen when nothing has been confirmed
+  if (listenForSelection === true) {
+    figma.ui.postMessage({
+      type: 'selected-count',
+      data: { count: figma.currentPage.selection.length }
+    });
+  }
+});
+
 // main listener on Figma scene side
 // https://www.figma.com/plugin-docs/api/figma-ui/#onmessage
 figma.ui.onmessage = async (msg) => {
   const { type } = msg;
-
-  // create rectangle example
-  // ///////////////////////////////////////////////////////////////////////////
-  if (type === 'create-rectangles') {
-    const nodes = [];
-
-    for (let i = 0; i < msg.count; i += 1) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-
-      figma.currentPage.appendChild(rect);
-
-      nodes.push(rect);
-    }
-
-    figma.currentPage.selection = nodes;
-
-    // https://www.figma.com/plugin-docs/api/figma-viewport/#scrollandzoomintoview
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
 
   // get selected count
   // ///////////////////////////////////////////////////////////////////////////
@@ -40,101 +37,10 @@ figma.ui.onmessage = async (msg) => {
     });
   }
 
-  // zoom into view
+  // get node info
   // ///////////////////////////////////////////////////////////////////////////
-  if (type === 'zoom-into-view') {
-    // https://www.figma.com/plugin-docs/api/figma-viewport/#scrollandzoomintoview
-    const testNode = figma.getNodeById('2:2');
-    const testNode2 = figma.getNodeById('6:8');
-
-    figma.viewport.scrollAndZoomIntoView([testNode, testNode2]);
-  }
-
-  // traversing
-  // ///////////////////////////////////////////////////////////////////////////
-  if (type === 'traversing') {
-    const [selectedNode] = figma.currentPage.selection;
-    const selectedNodeId = selectedNode.id;
-
-    // https://www.figma.com/plugin-docs/accessing-document/#traversing-all-nodes-in-the-page
-    const nodeWrapper = figma.getNodeById(selectedNodeId);
-    // console.log('nodeWrapper', nodeWrapper);
-
-    // make sure the node selected has child nodes
-    if (nodeWrapper.children === undefined) {
-      console.error('can only search on Nodes with children');
-    } else {
-      // find images in selected node/page
-      const imageNodes = nodeWrapper.findAll((node) => {
-        const { fills } = node;
-        // console.log('node', node);
-        // console.log('node.type', node.type);
-        // console.log('fills', fills);
-        // console.log('========');
-
-        const imageFills =
-          fills === undefined
-            ? []
-            : fills.filter((fill) => fill.type === 'IMAGE');
-
-        return imageFills.length > 0;
-      });
-
-      // loop through nodes that have images and grab bytes of image
-      const imagesData = await Promise.all(
-        imageNodes.map(async (image) => {
-          const { fills, id, name } = image;
-
-          // get the first fill that is an image type
-          const [imageFill] = fills.filter((fill) => fill.type === 'IMAGE');
-          const { imageHash } = imageFill;
-
-          // get image data by hash
-          // https://www.figma.com/plugin-docs/working-with-images/
-          const imageData = figma.getImageByHash(imageHash);
-          const bytes = await imageData.getBytesAsync();
-
-          // https://www.figma.com/plugin-docs/api/figma/#base64encode
-          const base64 = figma.base64Encode(bytes);
-
-          return {
-            base64,
-            id,
-            imageHash,
-            name
-          };
-        })
-      );
-
-      figma.ui.postMessage({
-        type: 'selected-images',
-        data: { images: imagesData }
-      });
-    }
-  }
-
-  // resize plugin
-  // ///////////////////////////////////////////////////////////////////////////
-  if (type === 'resize-plugin') {
-    const { height, width } = msg;
-
-    // https://www.figma.com/plugin-docs/api/figma-ui/#resize
-    figma.ui.resize(width, height);
-  }
-
-  // close plugin
-  // ///////////////////////////////////////////////////////////////////////////
-  if (type === 'close-plugin') {
-    // https://www.figma.com/plugin-docs/api/figma-ui/#close
-    figma.closePlugin();
+  if (type === 'get-node-info') {
+    listenForSelection = false;
+    console.log('msg', msg);
   }
 };
-
-// listener on selection change
-// https://www.figma.com/plugin-docs/api/properties/figma-on/
-figma.on('selectionchange', () => {
-  figma.ui.postMessage({
-    type: 'selected-count',
-    data: { count: figma.currentPage.selection.length }
-  });
-});
--- src/ui.js
+++ src/ui.js
@@ -4,140 +4,56 @@ import { createRoot } from 'react-dom/client';
 // scss base
 import './ui.scss';
 
-// icon(s)
-import SvgBox from './icons/Svg.Box';
-
 // function for message bridge to figma
 const sendToFigma = (type, data) => {
   parent.postMessage({ pluginMessage: { type, ...data } }, '*');
 };
 
-class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      selectedCount: null,
-      selectedImages: null
-    };
-
-    this.messageFromFigma = this.messageFromFigma.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('message', this.messageFromFigma);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('message', this.messageFromFigma);
-  }
+const App = () => {
+  const [selected, setSelected] = React.useState(0);
+  const [results, setResults] = React.useState(null);
 
   // main listener for all messages from Figma bridge
   // https://www.figma.com/plugin-docs/how-plugins-run/
-  messageFromFigma(event) {
+  const messageFromFigma = (event) => {
     const { data, type } = event.data.pluginMessage;
+    console.log('got message');
 
-    // selected count response
+    // on selection change
     if (type === 'selected-count') {
-      this.setState({
-        selectedCount: data.count
-      });
+      setSelected(data.count);
     }
+    console.log('type', type);
+    console.log('data', data);
+    console.log('===============');
+  };
 
-    if (type === 'selected-images') {
-      this.setState({
-        selectedImages: data.images
-      });
-    }
-  }
+  React.useEffect(() => {
+    window.addEventListener('message', messageFromFigma);
 
-  render() {
-    const { selectedCount, selectedImages } = this.state;
+    return () => {
+      window.removeEventListener('message', messageFromFigma);
+    };
+  }, []);
 
-    return (
-      <main>
-        <div className="header">
-          <SvgBox />
-          <h1>Examples</h1>
-        </div>
-
-        <div className="spacer-16" />
-
-        <section>
-          <h2>Reading the Figma Document</h2>
-
-          <div className="flex-row-space">
-            <button
-              className="brand"
-              onClick={() => sendToFigma('get-selected-count')}
-              type="button"
-            >
-              Get Selected Count
-            </button>
-
-            {selectedCount !== null && (
-              <p>{`Selected count: ${selectedCount}`}</p>
-            )}
-          </div>
-
+  return (
+    <main>
+      {selected === 0 && (
+        <React.Fragment>
+          <div className="flex-center-fill">please select a node</div>
           <div className="spacer-16" />
-
-          <button onClick={() => sendToFigma('zoom-into-view')} type="button">
-            Zoom into View Example
+          <button className="brand" type="submit">
+            Confirm
           </button>
+        </React.Fragment>
+      )}
 
-          {selectedCount === 1 && (
-            <React.Fragment>
-              <div className="spacer-16" />
+      {selected > 1 && <div>only select 1 node at a time</div>}
 
-              <button onClick={() => sendToFigma('traversing')} type="button">
-                Find Images | Traversing Example
-              </button>
-            </React.Fragment>
-          )}
-
-          {selectedImages && (
-            <React.Fragment>
-              <div className="spacer-16" />
-
-              <div className="flex-row-space flex-wrap">
-                {selectedImages.map((image) => {
-                  const { base64, id, name } = image;
-
-                  return (
-                    <img
-                      key={id}
-                      alt={name}
-                      className="preview-image"
-                      src={`data:image/png;base64,${base64}`}
-                    />
-                  );
-                })}
-              </div>
-            </React.Fragment>
-          )}
-        </section>
-
-        <div className="spacer-16" />
-
-        <button
-          onClick={() => {
-            sendToFigma('resize-plugin', { height: 500, width: 600 });
-          }}
-          type="button"
-        >
-          Resize Plugin: 500H by 600W
-        </button>
-
-        <div className="spacer-16" />
-
-        <button onClick={() => sendToFigma('close-plugin')} type="button">
-          Close Plugin
-        </button>
-      </main>
-    );
-  }
-}
+      {selected === 1 && <div>has selected 1</div>}
+    </main>
+  );
+};
 
 const container = document.getElementById('root');
 const root = createRoot(container);
